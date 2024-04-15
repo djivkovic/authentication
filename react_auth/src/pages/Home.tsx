a612dfe6607518260b1efb0c7f85e23169619cb5
djivkovic <92207833+djivkovic@users.noreply.github.com>
2024-04-15 16:09:14 +0200

    fixed link

--- react_auth/src/pages/Home.tsx
+++ react_auth/src/pages/Home.tsx
@@ -3,6 +3,12 @@ import { Link } from "react-router-dom";
 const Home = (props:{name:string, user_type:string}) => {
    
     return ( <><div>{props.name ? 'Hi ' + props.name + " " + (props.user_type) + '!': "You are not logged in..."}</div> 
-    {props.user_type  ? <Link className="editProfile" to={'edit-profile'}>Edit Profile</Link> : ''}</>);
+    {props.user_type  ? <Link className="editProfile" to={'edit-profile'}>Edit Profile</Link> : ''}
+    {props.user_type === "Accountant" ? (
+    <a className="accounting-ms" href="http://localhost:3001/">
+        Accountant MS
+    </a>
+    ) : null}
+    </>);
 }
 export default Home;
