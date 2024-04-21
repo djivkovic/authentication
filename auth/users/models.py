0bee8a097060b6da938126dca0a3e951c27095b8
djivkovic <92207833+djivkovic@users.noreply.github.com>
2024-04-22 01:45:16 +0200

    fixed hotelijers

--- auth/users/models.py
+++ auth/users/models.py
@@ -56,14 +56,12 @@ class User(AbstractBaseUser, PermissionsMixin):
     def has_module_perms(self, app_label):  
         return self.is_staff
 
-
-
-
 class Room(models.Model):
     id = models.AutoField(primary_key=True)
     room_number = models.DecimalField(max_digits=10, decimal_places=0)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     is_booked = models.BooleanField(default=False)
+    hotelijer = models.ForeignKey(User, on_delete=models.CASCADE, null=True) 
 
     def __str__(self):
         return f"Soba {self.room_number} - Cena: {self.price} - Zauzeta: {self.is_booked}"
@@ -100,9 +98,9 @@ class ModeratorProfile(models.Model):
     
 class HotelijerProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='hotelijer_profile')
-    hotelijer = models.CharField(max_length=100)
+    hotelijer = models.CharField(max_length=100, null=True)
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE, null=True)
+    rooms = models.ManyToManyField(Room, related_name='hotelijeri', null=True)
 
     def __str__(self):
-        return f"Profil hotela '{self.hotelijer}, Balance: {self.balance} , Room: {self.room}'"
+        return f"Profil hotela '{self.hotelijer}, Balance: {self.balance} , Rooms: {self.rooms.all()}'"
